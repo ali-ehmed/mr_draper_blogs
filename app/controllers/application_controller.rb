5c46ea6b4d09b4d151387bdc295cc6e24db8c433
Ali Ahmed <ali.ahmed.cs2014@gmail.com>
2019-08-05 12:27:56 +0500

    Remove after_sign_out_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,11 +34,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    # Overwriting the sign_out redirect path method
-    def after_sign_out_path_for(resource_or_scope)
-      request.referrer || root_path
-    end
-
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     end

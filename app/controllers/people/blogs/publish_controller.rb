4c581d881fa95c4769c66fcf0a6ff5bd158e61b7
Ali Ahmed <ali.ahmed.cs2014@gmail.com>
2019-08-07 11:18:15 +0500

    Add a small note for schedule later blog

--- app/controllers/people/blogs/publish_controller.rb
+++ app/controllers/people/blogs/publish_controller.rb
@@ -17,7 +17,11 @@ module People
       def create
         if publish_blog_params[:status] == Blog.statuses[:scheduled]
           @blog.schedule_for_later(publish_blog_params[:scheduled_at])
-          flash[:notice] = I18n.t('blogs.publish.scheduled', { scheduled_at: I18n.l(@blog.published_at, format: :default) })
+          flash[:notice] =
+            I18n.t(
+              'blogs.publish.scheduled',
+              { scheduled_at: I18n.l(@blog.published_at, format: :default) }
+            ) << schedule_note_in_dev_env
         else
           @blog.publish
           flash[:notice] = I18n.t('blogs.publish.published')
@@ -38,6 +42,12 @@ module People
 
         def publish_blog_params
           params.permit(:status, :scheduled_at)
+        end
+
+        def schedule_note_in_dev_env
+          return '' unless Rails.env.development?
+
+          I18n.t('blogs.publish.schedule_note')
         end
     end
   end
